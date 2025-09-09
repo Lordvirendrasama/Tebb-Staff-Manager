948de48d6de799bf205ef621ef9fcae86fa1779f
Viren Mirpuri <Viren389@gmail.com>
2025-09-09 14:47:53 +0000

    Try fixing this error: `Runtime Error: Error: Firebase Admin SDK is not

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,6 +1,7 @@
 import { config } from 'dotenv';
 config();
 
+import '@/lib/firebase';
 import '@/ai/flows/generate-consumption-report.ts';
 import '@/ai/flows/generate-attendance-report.ts';
 import '@/ai/flows/generate-leave-report.ts';
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -13,7 +13,7 @@ import {App, getApp as getAdminApp, getApps as getAdminApps} from 'firebase-admi
 const firebaseConfig: FirebaseOptions = {
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_Bucket,
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
