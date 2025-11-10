3fa99e59ad08f1cb2269cf4a2490c115e3416bbb
Viren Mirpuri <Viren389@gmail.com>
2025-11-10 12:23:22 +0000

    Cannot see marked as not worked/ mark as worked button here, Please add

--- src/components/attendance-editor.tsx
+++ src/components/attendance-editor.tsx
@@ -270,7 +270,7 @@ export function AttendanceEditor({ employees }: { employees: Employee[] }) {
                                     </DialogClose>
                                     <Button onClick={handleSaveChanges} disabled={isUpdating || !clockInTime || !clockOutTime}>
                                         {isUpdating ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : <Save className="mr-2 h-4 w-4" />}
-                                        Save
+                                        Save / Mark Worked
                                     </Button>
                                 </div>
                             </DialogFooter>
